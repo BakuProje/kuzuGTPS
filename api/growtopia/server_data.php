cbc4f32ab0ece0736fdd08f47f6a5a45d7a86d97
Baku <muhfaizsyeh@gmail.com>
2025-05-22 22:21:27 +0800

    Update server_data.php

--- api/growtopia/server_data.php
+++ api/growtopia/server_data.php
@@ -3,7 +3,7 @@ echo "server|206.189.91.118
 port|17091
 type|1
 #maint|Server under maintenance
-beta_server|206.189.91.118
+beta_server|127.0.0.1
 beta_port|17091
 meta|localhost
 RTENDMARKERBS1001";
